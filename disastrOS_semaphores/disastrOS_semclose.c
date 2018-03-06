aaeb76da6533a18dc0634ded724011e046506870
silviodeigiudici <silviodeigiudici@gmail.com>
2018-03-06 23:46:11 +0100

    using constants.h

--- disastrOS_semaphores/disastrOS_semclose.c
+++ disastrOS_semaphores/disastrOS_semclose.c
@@ -6,10 +6,8 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 #include "disastrOS_globals.h"
+#include "disastrOS_constants.h"
 
-#define ERRORRET -2 //memory error by free functions
-#define NOTFOUND -1 //semaphore with semnum passed it's not used by this process or doesn't exist
-#define OK 0
 
 void internal_semClose(){
 
@@ -18,19 +16,19 @@ void internal_semClose(){
 
   Semaphore* semaphore = SemaphoreList_byId(&semaphores_list, semnum);
   if(!semaphore){ //if semaphore == NULL it means that semaphore doesn't exist
-    running->syscall_retvalue = NOTFOUND;
+    running->syscall_retvalue = DSOS_ERESOURCENOFD;
     return;
   }
 
   SemDescriptor* semdescriptor = MySearch(&(running->sem_descriptors), semnum);
   if(!semdescriptor){ //check if there's the semaphore in this process
-    running->syscall_retvalue = NOTFOUND; //there aren't semaphore with id = semnum in this process
+    running->syscall_retvalue = DSOS_ERESOURCENOFD; //there aren't semaphore with id = semnum in this process
     return;
   }
 
   SemDescriptorPtr* semdescriptor_ptr = SemDescriptorPtrList_bySd(&(semaphore->descriptors), semdescriptor);
   if(!semdescriptor_ptr){ //this control it's not necessary (because if semdescriptor != NULL => semdescriptor_ptr != NULL)
-    running->syscall_retvalue = NOTFOUND;
+    running->syscall_retvalue = DSOS_ERESOURCENOFD;
     return;
   }
 
@@ -38,7 +36,7 @@ void internal_semClose(){
   semdescriptor = (SemDescriptor*) List_detach(&(running->sem_descriptors), (ListItem*) semdescriptor);
   ret = SemDescriptor_free(semdescriptor);
   if(ret != 0) {
-    running->syscall_retvalue = ERRORRET;
+    running->syscall_retvalue = DSOS_ERESOURCENOEXCL;
     return;
   }
 
@@ -46,7 +44,7 @@ void internal_semClose(){
   semdescriptor_ptr = (SemDescriptorPtr*) List_detach(&(semaphore->descriptors), (ListItem*) semdescriptor_ptr);
   ret = SemDescriptorPtr_free (semdescriptor_ptr);
   if(ret != 0) {
-    running->syscall_retvalue = ERRORRET;
+    running->syscall_retvalue = DSOS_ERESOURCENOEXCL;
     return;
   }
 
@@ -55,12 +53,12 @@ void internal_semClose(){
     semaphore = (Semaphore*) List_detach(&semaphores_list, (ListItem*) semaphore);
     ret = Semaphore_free(semaphore);
     if(ret != 0) {
-      running->syscall_retvalue = ERRORRET;
+      running->syscall_retvalue = DSOS_ERESOURCENOEXCL;
       return;
     }
 
   }
 
-  running->syscall_retvalue = OK;
+  running->syscall_retvalue = 0;
 
 }
--- disastrOS_semaphores/disastrOS_sempost.c
+++ disastrOS_semaphores/disastrOS_sempost.c
@@ -6,6 +6,7 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 #include "disastrOS_globals.h"
+#include "disastrOS_constants.h"
 
 
 void internal_semPost(){
@@ -14,16 +15,17 @@ void internal_semPost(){
 
   SemDescriptor* semdescriptor = (SemDescriptor*) SemDescriptorList_byFd(&(running->sem_descriptors), fd);
   if(!semdescriptor){ //semdescriptor doesn't exist
-    running->syscall_retvalue = -1;
+    running->syscall_retvalue = DSOS_ERESOURCENOFD;
     return;
   }
 
   Semaphore* semaphore = semdescriptor->semaphore; //semaphore has to exist if semdescriptor exist
 
   if(semaphore->count < 0){
+    
     //take head descriptor in semaphore's waiting_descriptors
     SemDescriptorPtr* head_wait_descriptor = (SemDescriptorPtr*) List_detach(&(semaphore->waiting_descriptors), (ListItem*) (semaphore->waiting_descriptors).first);
-    //head_wait_descriptor can't be == NULL if count < 0 because has to exist one process that wait
+    //head_wait_descriptor can't be NULL if count < 0 because has to exist one process that wait
 
     //drop pcb of process with head_wait_descriptor from waiting_list and put it in ready_list
     PCB* pcb_head = head_wait_descriptor->descriptor->pcb;
